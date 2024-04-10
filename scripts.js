ffae94a6ad62751157e723599d9602ec72ee50b0
Hannah Chang <99717307+HCstarCoder34@users.noreply.github.com>
2024-04-09 23:46:19 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+/*!
+* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+*/
+
+function doClick() {
+    alert("You have added this item to your cart");
+
+}
+
+
+
+
+
+    
+
